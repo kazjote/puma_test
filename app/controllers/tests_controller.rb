b35a85b3d5edc02ea46a8d34aca9f198f125b3c2
kazjote <kazjote@gmail.com>
2013-03-08 17:59:00 +0100

    Initial commit

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,9 @@
+class TestsController < ApplicationController
+  def test1
+    render text: "test1"
+  end
+
+  def test2
+    render text: "test2"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreshRailsPuma::Application.config.secret_token = '28aa39f8d12c43920f4e5d777168e5660ba1ca5da0855d6d2df2fc264b7951f8fa676d02d93d697c0a511e7d851ded926137851c94e1635be322ed34e5e4b883'
